ead3f2339d3b8009691f4e4ee68d584a2c0dbd29
Nick Hodges <nickhodges@Nicks-MBP.home>
2023-07-11 16:25:45 -0400

    task2

---
+++ task2/src/app/todo/todo.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Todo } from './todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+  todos: Todo[] = [];
+  newTodo: string;
+
+  saveTodo(){
+    if(this.newTodo){
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      this.todos.push(todo);
+      this.newTodo = '';
+    } else {
+      alert('Please Enter Item');
+    }
+  }
+
+  removeTodo(id:number){
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+}
